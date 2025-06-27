cac6b5fde2ce1868a476ab644b40ed5105965919
alexhr000 <alexhrmessage@gmail.com>
2025-06-27 12:07:03 +0300

    fix: issues

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 export const DEFAULT_RPC = 'https://rpc.solpush.io'; 
 export const DIVERS_ADDRESS = 'GxkB4oYYLsoeAoxAdXjDEBSrP7JGCy3re7mqozFYyiYW'; 
+export const DIVERS_RPC = 'https://rpc.solpush.io'; 
+export const RPC_PAYMENT_LAMPORTS = 0.0001 * LAMPORTS_PER_SOL; 
